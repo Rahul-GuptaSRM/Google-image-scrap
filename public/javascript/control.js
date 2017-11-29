b951dc6718e5c429f63f8bc772922561d0801efb
Rahul <iamrahulgupta231@gmail.com>
2017-11-29 18:57:40 +0530

    Update control.js

--- public/javascript/control.js
+++ public/javascript/control.js
@@ -1,34 +1,7 @@
-var app = angular.module('main',['ngRoute']);
+ var app = angular.module('main', ['ngRoute', 'ngStorage']);
 
-/*app.service('userService', function(){
-	this.name = "";
-	this.img = "";
-});*/
 
-app.service('userService', function() {
-
-    // variables
-    var name = null;
-    var img = null
-                    
-    return {
-        // get compnay
-        getname: function() {
-            return name;
-        },
-        getimg: function(){
-        	return img;
-        },
-        setname: function(val)
-        {
-        	name = val
-        },
-        // set company
-        setimg: function(value) {
-            img = value;
-        }
-    }
-});
+/*-------------ng-routing----------------*/
 app.config(function($routeProvider)
 {
 	$routeProvider
@@ -58,14 +31,14 @@ app.config(function($routeProvider)
 	   redirectTo: '/'
 	   });
 });
-
-app.controller('getdata',function($scope,$http,userService,$location)
+/*----------------------end of ng-routing-------------------*/
+app.controller('getdata',function($scope,$http,$location,$localStorage)
 {
 	$scope.initialize = function()
 	{
 		var data = {
 			method : 'GET',
-			url : 'http://localhost:8080/api/getdata',
+			url : '/api/getdata',
             headers:{
             	'Accept':'application/json',
             	'Content-type' : 'application/json;charset=utf-8'
@@ -75,14 +48,9 @@ app.controller('getdata',function($scope,$http,userService,$location)
 		$http(data).then(function success(response)
 {
 $scope.events = response.data;
-var str = response.data.name;
-var newstr = str.substring(0, str.indexOf("@"));
-//console.log(newstr);
-//userService.name = response.data.name;
+$localStorage.username = response.data.name;
 
 userService.setname(str);
-
-console.log(userService.getname());
 },
 function error(response)
 {
@@ -93,28 +61,25 @@ function error(response)
 
 $scope.getimage = function(id)
 	{
-		userService.setimg(id);
+		$localStorage.img = id; //putting the clicked search in local storage. In case if user reload the browser, the clicked search would not get lost 
 
-$location.path("/display");
+
+$location.path("/display"); //after clicking  on search, shift to do a display page
 	};
 
 	$scope.calculation = function(){
-		var n = userService.getname();
-		var p = userService.getimg();
-		//console.log(n+" "+p);
+		var n = $localStorage.username;
+		var p = $localStorage.img
 		var add = n+'_'+p+'_';
-		//console.log(add);
 		var add_arr = [];
 		if( p!= null)
 		{
 		for(var i=0;i<15;i++)
 		{
 			add_arr[i] = add+i+'.jpg';
-		//	console.log(add_arr[i]);
 		}
 	}
 		$scope.images = add_arr;
-		//console.log($scope.images);
 	}
 
 });
